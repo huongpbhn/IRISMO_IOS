01f0d9ed6a6f1ce5dacd5a6c210e8ebeca84ec47
ricky-ir <ricky@interush.net>
2015-09-01 12:01:06 -0700

    fix ShareActivityController, added CheckSystem

---
+++ System/CheckSystem/CheckSystem.h
@@ -0,0 +1,19 @@
+//
+//  CheckSystem.h
+//  Academy
+//
+//  Created by Ricky Lee on 9/1/15.
+//  Copyright (c) 2015 Ricky Lee. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+#define SYSTEM_VERSION_EQUAL_TO(v)                  ([[[UIDevice currentDevice] systemVersion] compare:v options:NSNumericSearch] == NSOrderedSame)
+#define SYSTEM_VERSION_GREATER_THAN(v)              ([[[UIDevice currentDevice] systemVersion] compare:v options:NSNumericSearch] == NSOrderedDescending)
+#define SYSTEM_VERSION_GREATER_THAN_OR_EQUAL_TO(v)  ([[[UIDevice currentDevice] systemVersion] compare:v options:NSNumericSearch] != NSOrderedAscending)
+#define SYSTEM_VERSION_LESS_THAN(v)                 ([[[UIDevice currentDevice] systemVersion] compare:v options:NSNumericSearch] == NSOrderedAscending)
+#define SYSTEM_VERSION_LESS_THAN_OR_EQUAL_TO(v)     ([[[UIDevice currentDevice] systemVersion] compare:v options:NSNumericSearch] != NSOrderedDescending)
+
+@interface CheckSystem : NSObject
+
+@end
