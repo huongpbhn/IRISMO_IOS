e71589e20ac141e40ddbb25b20e4bfb4b935d3c5
ricky-ir <ricky@interush.net>
2015-09-11 16:50:41 -0700

    update check system

--- System/CheckSystem/CheckSystem.h
+++ System/CheckSystem/CheckSystem.h
@@ -14,10 +14,16 @@
 #define SYSTEM_VERSION_LESS_THAN(v)                 ([[[UIDevice currentDevice] systemVersion] compare:v options:NSNumericSearch] == NSOrderedAscending)
 #define SYSTEM_VERSION_LESS_THAN_OR_EQUAL_TO(v)     ([[[UIDevice currentDevice] systemVersion] compare:v options:NSNumericSearch] != NSOrderedDescending)
 
-#define IS_IPHONE ( [[[UIDevice currentDevice] model] isEqualToString:@"iPhone"] )
+#define IS_IPHONE (UI_USER_INTERFACE_IDIOM() == UIUserInterfaceIdiomPhone)
 #define IS_IPOD   ( [[[UIDevice currentDevice ] model] isEqualToString:@"iPod touch"] )
-#define IS_HEIGHT_GTE_568 [[UIScreen mainScreen ] bounds].size.height >= 568.0f
-#define IS_IPHONE_5 ( IS_IPHONE && IS_HEIGHT_GTE_568 )
+#define SCREEN_HEIGHT [[UIScreen mainScreen ] bounds].size.height
+#define IS_IPHONE_4 ( IS_IPHONE && SCREEN_HEIGHT < 568.0)
+#define IS_IPHONE_5 ( IS_IPHONE && SCREEN_HEIGHT == 568.0)
+#define IS_IPHONE_6 ( IS_IPHONE && SCREEN_HEIGHT == 667.0)
+#define IS_IPHONE_6P ( IS_IPHONE && SCREEN_HEIGHT == 736.0)
+
+
+
 
 @interface CheckSystem : NSObject
 
